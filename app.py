8d73f8acebbe37a9a111ebfc1915a4c527205013
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-05-25 16:55:18 +0100

    Removing unused code/libraries for MVP

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask, redirect, url_for, request, make_response
 import asyncio
 
 from functools import wraps
-from authlib.integrations.flask_client import OAuth
-from six.moves.urllib.parse import urlencode
-import jwt
 import datetime
 import uuid
 
@@ -13,8 +10,6 @@ import toml
 import logging
 
 
-from common import check_user, requires_privilege, templated, get_hostname
-from common.blueprints.auth import auth_handlers
 from common.providers import sendGridProvider, send_email
 
 
@@ -22,36 +17,9 @@ from models import Person, Organization
 
 
 app = Flask(__name__)
-app.secret_key = app.config['SECRET_KEY']
+
 # load the config file from the TOML formatted file (not checked into repository)
 app.config.from_file('config.toml', toml.load)
-app.register_blueprint(auth_handlers, url_prefix='/auth')
-
-app.config['NAVELEMENTS'] = {}
-
-app.config['NAVSET'] = {
-    'holding':[],
-    'normal':[]
-}
-
-
-app.auth0 = OAuth(app).register(
-    'auth0',
-    client_id = app.config['AUTH0_CLIENT_ID'],
-    client_secret = app.config['AUTH0_CLIENT_SECRET'],
-    api_base_url = app.config['AUTH0_API_BASE_URL'],
-    access_token_url = app.config['AUTH0_ACCESS_TOKEN_URL'],
-    authorize_url = app.config['AUTH0_AUTHORIZE_URL'],
-    client_kwargs={
-        'scope': 'openid profile email',
-    },
-)
-
-@app.get('/')
-@check_user
-@templated('home')
-def home_handler(userobj):
-    return {'message': uuid.uuid4().hex, 'host':get_hostname(request)}
 
 
 @app.get('/schema')
@@ -64,6 +32,7 @@ def schema_handler():
 def test_handler():
     asyncio.run(send_email('signup', ['chris@wearecast.org.uk'], 'Welcome to Coffee Connections', {'first_name':'Chris', 'hostname':'http://localhost:5000', 'uid': uuid.uuid4().hex}))
     return 'hello'
+
 
 @app.get('/users/add')
 def signup_handler():
@@ -84,78 +53,37 @@ def confirm_user_handler(uuid):
     return 'email confirmation'
 
 
-@app.get('/admin')
-@requires_privilege('admins')
-@templated('admin/home')
-def admin_home_handler(userobj):
-    # TODO think about what this should contain? Stats? Recent actions?
-    return 'admin home'
-
-@app.get('/admin/users')
-@requires_privilege('admins')
-def admin_users_handler(userobj):
-    # TODO list of users with sorting
-    return 'admin/ list users'+ ' | ' + userobj['given_name']
-
-
-@app.get('/admin/users/edit/<string:uid>')
-@requires_privilege('admins')
-def admin_user_edit_handler(userobj, uid):
-    # TODO edit of a user of coffee connections
-    return 'admin/ edit user' + uid + ' | ' + userobj['given_name']
-
-
-@app.get('/admin/users/view/<string:uid>')
-@requires_privilege('admins')
-def admin_user_view_handler(userobj, uid):
-    # TODO view of a user of coffee connections
-    return 'admin/ view user' + uid + ' | ' + userobj['given_name']
-
 
 @app.get('/admin/users/approve/<string:uid>')
-@requires_privilege('admins')
 def admin_user_approve_handler(userobj, uid):
     # TODO approve of a user of coffee connections
     return 'admin/ approve user' + uid + ' | ' + userobj['given_name']
 
 
 @app.get('/admin/matches/create')
-@requires_privilege('admins')
 # TODO think about what this does. Does it create a new match table? Does it populate with guessed matches
 def admin_create_matches_handler(userobj):
     return 'admin/ create matches '+ ' | ' + userobj['given_name']
 
 
 @app.get('/admin/matches/view')
-@requires_privilege('admins')
 def admin_view_matches_handler(userobj):
     # TODO show matches
     return 'admin/ view matches '+ ' | ' + userobj['given_name']
 
 
-@app.get('/admin/matches/edit')
-@requires_privilege('admins')
-def admin_edit_matches_handler(userobj):
-    # TODO edit matches
-    return 'admin/ edit matches '+ ' | ' + userobj['given_name']
-
-
 @app.get('/admin/matches/test')
-@requires_privilege('admins')
 def admin_test_matches_handler(userobj):
     # TODO show form which will approve the sending, make it have some friction
         return 'admin/ send test matches '+ ' | ' + userobj['given_name']
 
 
 @app.post('/admin/matches/test')
-@requires_privilege('admins')
 def admin_test_matches_confirm_handler(userobj):
     return 'admin/ send test matches confirmed '+ ' | ' + userobj['given_name']
 
 
-# TODO email permission decorator
 @app.get('/admin/matches/send')
-@requires_privilege('senders')
 def admin_send_matches_handler(userobj):
     # TODO show form which will approve the sending, make it have some friction
     return 'admin/ send live matches '+ ' | ' + userobj['given_name']
@@ -163,27 +91,23 @@ def admin_send_matches_handler(userobj):
 
 # TODO email permission decorator
 @app.post('/admin/matches/send')
-@requires_privilege('senders')
 def admin_send_matches_confirm_handler(userobj):
     return 'admin/ send live matches confirmed '+ ' | ' + userobj['given_name']
 
 
 @app.get('/admin/followups/test')
-@requires_privilege('admins')
 def admin_test_followups_handler(userobj):
     # TODO show form which will approve the sending, make it have some friction
         return 'admin/ send test matches '+ ' | ' + userobj['given_name']
 
 
 @app.post('/admin/followups/test')
-@requires_privilege('admins')
 def admin_test_followups_confirm_handler(userobj):
     return 'admin/ send test matches confirmed '+ ' | ' + userobj['given_name']
 
 
 # TODO email permission decorator
 @app.get('/admin/followups/send')
-@requires_privilege('senders')
 def admin_send_followups_handler(userobj):
     # TODO show form which will approve the sending, make it have some friction
     return 'admin/ send live matches '+ ' | ' + userobj['given_name']
@@ -191,7 +115,6 @@ def admin_send_followups_handler(userobj):
 
 # TODO email permission decorator
 @app.post('/admin/followups/send')
-@requires_privilege('senders')
 def admin_send_followups_confirm_handler(userobj):
     return 'admin/ send live matches confirmed '+ ' | ' + userobj['given_name']
 
