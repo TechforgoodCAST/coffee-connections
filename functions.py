8664f63cc565e1d7b41cf1dd5d96d9f6e380dbb4
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-05-26 16:35:16 +0100

    Added some comments and type hints and a function to map the historic field names to the new schema.org derived ones

--- functions.py
+++ functions.py
@@ -1,7 +1,17 @@
 
+from typing import Dict, List
 _models = __import__("models")
 
-def get_fields(modelname):
+def get_fields(modelname:str) -> List:
+    """
+    This function takes the name of a model and returns a list of all the fields (so that form input can be captured for example)
+    
+    Args:
+        modelname (str): the name of the model
+    
+    Returns:
+        List: an array of the field names
+    """
     _model = getattr(_models,modelname)
     model = _model({}).as_schema()
     print (model)
@@ -15,3 +25,36 @@ def get_fields(modelname):
         else:
             fields.append(field)
     return fields
+
+
+
+
+def map_historic_data_fields(fields:List) -> List:
+    """
+    This function takes a list of historic field names from the Coffee Connections database and returns a list of the schema.org derived field names
+    
+    Args:
+        fields (List): the fields to be convered
+
+    Returns:
+        List : mapped fields, a list of the schema.org derived field names
+    """
+    mapping = {
+        'id': 'cc__id',
+        'first_name': 'given_name',
+        'last_name': 'family_name',
+        'org': 'works_for-name',
+        'email':'email',
+        'domain': 'works_for-identifier',
+        'job_title': 'job_title',
+        'bio': 'description',
+        'region': 'work_location',
+        'consent_text': 'cc__consent_text',
+        'digital_journey': 'cc__digital_journey',
+        'created_at': 'cc__created_at',
+        'updated_at': 'cc__updated_at',
+        'status': 'cc__status',
+        'consent': 'cc__consent'
+    }
+    mapped_fields = [mapping[field] for field in fields]
+    return mapped_fields
