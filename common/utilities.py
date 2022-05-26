43944424be1aafac12cbddfe770270f684e27d2d
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-05-26 12:27:53 +0100

    Adding the request variables function (from Grant Advisor)

--- common/utilities.py
+++ common/utilities.py
@@ -1,6 +1,6 @@
 from typing import Callable
 from typing import List, Dict, Optional, Callable, Union
-from flask import current_app
+from flask import current_app, request
 
 
 """
@@ -12,3 +12,39 @@ Args:
 def get_hostname(request : Callable) -> str:
     return request.url_root
 
+
+
+def request_variables(form, params=None):
+    if not params:
+        params = [element for element in form['elements']]
+    variables = {}
+    default_value = None
+    for param in params:
+        variables[param] = None
+        if request.method == "GET":
+            variables[param] = request.args.get(param)
+        else:
+            if 'multi' in form['elements'][param]:
+                variables[param] = request.values.getlist(param)
+            else:
+                try:
+                    variables[param] = request.form[param]
+                    if form:
+                        if form['elements'][param]['display'] == 'textarea':
+                            variables[param] = '\n'.join(request.form[param].splitlines())
+                        elif form['elements'][param]['display'] == 'text':
+                            variables[param] = ' '.join(request.form[param].splitlines())
+                except:
+                    try:
+                        variables[param] = request.get_json()[param]
+                    except:
+                        variables[param] = None
+        if form:
+            if 'null_text' in form['elements'][param]:
+                if variables[param] == form['elements'][param]['null_text']:
+                    variables[param] = None
+        if variables[param] is not None and len(variables[param]) == 0:
+            variables[param] = None
+        if variables[param] == 'None':
+            variables[param] = None
+    return variables
